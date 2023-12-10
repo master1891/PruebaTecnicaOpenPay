c011265664f4368a1c8171fd7c62b6b03b2687bb
Nelson Ek <nelson.ek@macropay.mx>
2023-12-10 08:54:28 -0600

    Se agrega mejora en el diseño del HomeProfile

--- app/src/main/java/com/nels/master/pruebaopenpay/features/homefeature/HomeScreen.kt
+++ app/src/main/java/com/nels/master/pruebaopenpay/features/homefeature/HomeScreen.kt
@@ -1,11 +1,30 @@
 package com.nels.master.pruebaopenpay.features.homefeature
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.nels.master.pruebaopenpay.R
 import com.nels.master.pruebaopenpay.features.homefeature.network.models.response.profile.ProfileResponse
 import com.nels.master.pruebaopenpay.features.homefeature.viewmodels.ProfileStatus
 import com.nels.master.pruebaopenpay.features.homefeature.viewmodels.ProfileViewModel
@@ -29,7 +48,7 @@ fun HomeScreen(profileViewModel: ProfileViewModel) {
     ) {
 
         if (profileUser.state == ProfileStatus.Success )
-            HeaderProfile(profileResponse = profileUser.dataProfile)
+            HeaderProfile(profileResponse = profileUser.dataProfile, modifier =Modifier )
         if (favoritesUserState.state == ProfileStatus.Success)
             Carrousel(favoritesUserState.moviesResponse!!.results,"Tus peliculas favoritas")
         if (ratedUserState.state == ProfileStatus.Success)
@@ -38,8 +57,26 @@ fun HomeScreen(profileViewModel: ProfileViewModel) {
 }
 
 @Composable
-fun HeaderProfile(profileResponse: ProfileResponse?) {
+fun HeaderProfile(profileResponse: ProfileResponse?,modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth().padding(top = 40.dp),
+       verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Image(
+            imageVector = Icons.Default.Person,
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(70.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Gray, CircleShape)
+        )
+    }
     profileResponse?.let {
-        Text(text = profileResponse.username)
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Hola ${profileResponse.username}", textAlign = TextAlign.Center)
     }
 }
